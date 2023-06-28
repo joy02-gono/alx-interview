9dd24dfed7b9cbcea415fcc52d50fcfc13b7d42f
joy02-gono <rejoicegono@gmail.com>
2023-06-28 12:02:40 -0700

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,27 @@
+#!/usr/bin/python3
+"""Pascal Triangle Interview """
+
+
 def pascal_triangle(n):
+    """returns a list of numbers which
+    represents the pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for j in range(1, n):
-        previous = triangle[j - 1]
-        current = [1]
-        for k in range(1, j):
-            current.append(previous[k - 1] + previous[k])
-        current.append(1)
-        triangle.append(current)
+    pascal_triangle = [0] * n
 
-    return triangle
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        current_row = [0] * (i+1)
+        current_row[0] = 1
+        current_row[len(current_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(current_row):
+                y = pascal_triangle[i - 1][j]
+                z = pascal_triangle[i - 1][j - 1]
+                current_row[j] = y + z
+
+        pascal_triangle[i] = current_row
+
+    return pascal_triangle
